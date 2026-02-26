a41cb997fe1925217d369a349cb2e43dcf028693
Daniel Onís Fabián <danielonisfabian@MacBook-Air-de-Daniel-277.local>
2026-02-26 13:24:49 +0100

    try out Azure connection

--- users/users-service.js
+++ users/users-service.js
@@ -18,6 +18,8 @@ const Stats = require("./models/stats");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/userdb"
 const connectToMongoDB = async () => {
   try {
+    console.log("Conectando...")
+    console.log(MONGODB_URI)
     await mongoose.connect(MONGODB_URI)
     console.log('Connected to MongoDB')
   } catch (err) {
