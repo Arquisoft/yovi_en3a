300f9513acc4f329fe3beb0b37c4536d998dc576
SSergio0-UO <uo300084@uniovi.es>
2026-03-04 16:30:10 +0100

    Menu implementado, pero genera problemas con la landing page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,11 +3,14 @@ import './App.css'
 import RegisterForm from './RegisterForm';
 import LoginForm from './LoginForm';
 import Landing from './Landing';
+import MenuView from './MenuView';
 
-type AuthScreen = 'landing' | 'login' | 'register';
+type AuthScreen = 'landing' | 'login' | 'register' | 'menu';
 
 function App() {
   const [currentScreen, setCurrentScreen] = useState<AuthScreen>('landing');
+  // Para probar el menu sin el login, deberia de estar comentado y la linea superior descomentada
+  //const [currentScreen, setCurrentScreen] = useState<AuthScreen>('menu');
   const [status, setStatus] = useState<string>('Cargando...');
 
   useEffect(() => {
@@ -19,23 +22,30 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Welcome to Yovi</h2>
-      <p>{"Server status: " + status}</p>
+      {/* <h2>Welcome to Yovi</h2> */}
+      {/*<p>{"Server status: " + status}</p>*/}
 
       {currentScreen === 'landing' && (
-        <Landing 
+        <Landing
           onSelectLogin={() => setCurrentScreen('login')}
           onSelectRegister={() => setCurrentScreen('register')}
         />
       )}
 
       {currentScreen === 'login' && (
-        <LoginForm onSwitchToRegister={() => setCurrentScreen('register')} />
+        <LoginForm onSwitchToRegister={() => setCurrentScreen('register')}
+          onLoginSuccess={() => setCurrentScreen('menu')}
+        />
       )}
 
       {currentScreen === 'register' && (
         <RegisterForm onSwitchToLogin={() => setCurrentScreen('login')} />
       )}
+
+      {currentScreen === 'menu' && (
+        <MenuView onLogout={() => setCurrentScreen('landing')} />
+      )}
+
     </div>
   );
 }
