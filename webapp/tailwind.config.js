6a5497fd4464b55e9403327c3cdfcfd8cf2b020e
gonza <UO294665@uniovi.es>
2026-03-05 13:42:09 +0100

    Fix in tailwind.config.js

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,9 +1,11 @@
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
     content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // all your React files
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
   	extend: {
@@ -56,5 +58,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 };
